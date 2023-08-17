9e714bc42064a6b8346aa8e2c2923a3b355d212b
0476a <0476a@naver.com>
2023-08-18 01:13:42 +0900

    2023.08.18(v-bind 디렉티브 속성 바인딩 하기)

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
     data() {
         return {
-            courseGoal: '강의를 수료해 Vue 배우기!'
+            courseGoal: '강의를 수료해 Vue 배우기!',
+            vueLink: 'https://vuejs.org/'
         };
     }
 });
