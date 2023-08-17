881f5a4e92a040b7c79d7233d71e6e51d5530428
0476a <0476a@naver.com>
2023-08-18 01:38:35 +0900

    2023.08.18(Vue 앱 내에서 데이터 작업하기)

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
     data() {
         return {
-            courseGoal: '강의를 수료해 Vue 배우기!',
+            courseGoalA: '강의를 수료해 Vue 배우기!',
+            courseGoalB: 'Vue를 마스터해서 멋진 앱을 만드세요!',
             vueLink: 'https://vuejs.org/'
         };
     },
@@ -9,9 +10,9 @@ const app = Vue.createApp({
         outputGoal() {
             const randomNumber  = Math.random();
             if (randomNumber < 0.5) {
-                return 'Learn Vue!';
+                return this.courseGoalA;
             } else {
-                return 'Master Vue!';
+                return this.courseGoalB;
             }
         }
     }
